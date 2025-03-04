4c86596b9767748e8b5eae8c78ed03a889a7fe12
PrajwalBendale <prajwalbendale872@gmail.com>
2025-03-04 12:38:07 +0530

    changes done

---
+++ src/main/java/com/edsom/EraPay/Dtos/DepositCoinsDto.java
@@ -0,0 +1,10 @@
+package com.edsom.EraPay.Dtos;
+
+import java.time.LocalDateTime;
+
+public class DepositCoinsDto {
+
+    Double coins;
+    LocalDateTime depositTime;
+    String depositedFromWallet;
+}
